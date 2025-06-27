4604b2f9521196f080188ed30cd00a04ed634aba
ErgoMundusDovi <eddylou91@gmail.com>
2025-06-27 12:45:27 +0200

    ...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://back-end-63xf.vercel.app/year')
 .then(response=> response.json()
 .then(data=>{
-    document.querySelector('.date').innerHTML += `<p>${data}</p>`
+    document.querySelector('#date').innerHTML += ` <h1 class="date">${data}</h1>`
 }))
